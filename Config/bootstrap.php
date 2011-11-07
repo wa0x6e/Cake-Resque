4b96d9187a7f707ba5a2cae1c80270fb1cff1fad
Kamisama <kami@kamisama.me>
2011-11-07 18:46:32 -0500

    QUEUE argument takes a string instead of array

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -2,7 +2,7 @@
 
 	 Configure::write('Resque', array(
 		'Redis' => array('host' => 'localhost', 'port' => 6379),	// Redis server location
-		'queue' => array('default'),								// Name of the default queue
+		'queue' => 'default',								// Name of the default queue
 		'interval' => 5,											// Number of second between each works
 		'count' => 1												// Number of forks for each workers
 	));
