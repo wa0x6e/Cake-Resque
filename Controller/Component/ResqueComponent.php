8cc8ccd7b59853660c67668c47a902b9f220ba24
Kamisama <kami@kamisama.me>
2012-08-26 18:31:22 -0400

    Load Resque Plugin with bootstrap setting

--- Controller/Component/ResqueComponent.php
+++ Controller/Component/ResqueComponent.php
@@ -1,11 +1,10 @@
 <?php
 
-require_once APP . 'Plugin' . DS . 'Resque' . DS . 'Vendor' . DS . 'php-resque' . DS . 'lib' . DS . 'Resque.php';
+require_once App::pluginPath('Resque') . 'vendor' . DS . Configure::read('Resque.Resque.lib') . DS . 'lib' . DS . 'Resque.php';
 
 class ResqueComponent extends Component {
 
 	public function __construct(ComponentCollection $collection, $settings = array()) {
 		Resque::setBackend(Configure::read('Resque.Redis.host') . ':' . Configure::read('Resque.Redis.port'));
 	}
-
 }
