837632a2673aa860415fad47bb7632e2b1cd1ac0
Brian Smith <bdsmith@jpl.nasa.gov>
2013-07-21 12:04:29 -0700

    Allow configurable vendor path for Composer packages
    The current install steps assume Composer is run inside the plugin folder and installs packages in /Plugin/CakeResque/vendor. This change allows you to also install the CakeResque plugin with Composer. The plugin and all its dependencies are installed in the default Composer location (/Vendor for CakePHP). The default behavior stays the same as it was before.

--- Lib/CakeResque.php
+++ Lib/CakeResque.php
@@ -23,8 +23,8 @@ if (substr(Configure::read('CakeResque.Resque.lib'), 0, 1) === '/') {
 	require_once Configure::read('CakeResque.Resque.lib') . DS . 'lib' . DS . 'Resque.php';
 	require_once Configure::read('CakeResque.Resque.lib') . DS . 'lib' . DS . 'Resque' . DS . 'Worker.php';
 } else {
-	require_once realpath(App::pluginPath('CakeResque') . 'vendor' . DS . Configure::read('CakeResque.Resque.lib') . DS . 'lib' . DS . 'Resque.php');
-	require_once realpath(App::pluginPath('CakeResque') . 'vendor' . DS . Configure::read('CakeResque.Resque.lib') . DS . 'lib' . DS . 'Resque' . DS . 'Worker.php');
+	require_once realpath(Configure::read('CakeResque.Vendor.path') . DS . Configure::read('CakeResque.Resque.lib') . DS . 'lib' . DS . 'Resque.php');
+	require_once realpath(Configure::read('CakeResque.Vendor.path') . DS . Configure::read('CakeResque.Resque.lib') . DS . 'lib' . DS . 'Resque' . DS . 'Worker.php');
 }
 
 
@@ -33,12 +33,12 @@ if (substr(Configure::read('CakeResque.Scheduler.lib'), 0, 1) === '/') {
 	require_once Configure::read('CakeResque.Scheduler.lib') . DS . 'lib' . DS . 'ResqueScheduler' . DS . 'Stat.php';
 	require_once Configure::read('CakeResque.Scheduler.lib') . DS . 'lib' . DS . 'ResqueScheduler' . DS . 'Job' . DS . 'Status.php';
 } else {
-	require_once realpath(App::pluginPath('CakeResque') . 'vendor' . DS . Configure::read('CakeResque.Scheduler.lib') . DS . 'lib' . DS . 'ResqueScheduler' . DS . 'ResqueScheduler.php');
-	require_once realpath(App::pluginPath('CakeResque') . 'vendor' . DS . Configure::read('CakeResque.Scheduler.lib') . DS . 'lib' . DS . 'ResqueScheduler' . DS . 'Stat.php');
-	require_once realpath(App::pluginPath('CakeResque') . 'vendor' . DS . Configure::read('CakeResque.Scheduler.lib') . DS . 'lib' . DS . 'ResqueScheduler' . DS . 'Job' . DS . 'Status.php');
+	require_once realpath(Configure::read('CakeResque.Vendor.path') . DS . Configure::read('CakeResque.Scheduler.lib') . DS . 'lib' . DS . 'ResqueScheduler' . DS . 'ResqueScheduler.php');
+	require_once realpath(Configure::read('CakeResque.Vendor.path') . DS . Configure::read('CakeResque.Scheduler.lib') . DS . 'lib' . DS . 'ResqueScheduler' . DS . 'Stat.php');
+	require_once realpath(Configure::read('CakeResque.Vendor.path') . DS . Configure::read('CakeResque.Scheduler.lib') . DS . 'lib' . DS . 'ResqueScheduler' . DS . 'Job' . DS . 'Status.php');
 }
 
-require_once realpath(App::pluginPath('CakeResque') . 'vendor' . DS . 'kamisama' . DS . 'resque-status' . DS . 'src' . DS . 'ResqueStatus' . DS . 'ResqueStatus.php');
+require_once realpath(Configure::read('CakeResque.Vendor.path') . DS . 'kamisama' . DS . 'resque-status' . DS . 'src' . DS . 'ResqueStatus' . DS . 'ResqueStatus.php');
 
 Resque::setBackend(
 	Configure::read('CakeResque.Redis.host') . ':' . Configure::read('CakeResque.Redis.port'),
