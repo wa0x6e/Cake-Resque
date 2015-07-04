351fc76bbaa7256259b43897f8f807699e2d54b8
hooman naghiee <hooman@rtler.com>
2015-07-04 17:32:35 +0430

    Fix the require path of CakeResque.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -18,6 +18,6 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-require_once dirname(__DIR__) . DS . '..' . DS . 'CakeResque.php';
+require_once dirname(__DIR__) . DS . 'CakeResque.php';
 
 CakeResque::init();
