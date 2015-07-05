6d56f21e4e9b5b6c7bfa0611c10c9310da5b3bd0
hooman naghiee <hooman@rtler.com>
2015-07-05 10:58:03 +0430

    use Cake\Core\Plugin

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php
+use Cake\Core\Plugin;
+
 /**
  * CakeResque configuration file
  *
@@ -65,136 +67,136 @@
  * @see CakeResque::init(), CakeResque::loadConfig().
  */
 $config['CakeResque'] = [
-	'Redis'     => [
-		'host'      => 'localhost',        // Redis server hostname
-		'port'      => 6379,                // Redis server port
-		'database'  => 0,            // Redis database number
-		'namespace' => 'resque',    // Redis keys namespace
-		'password'  => null            // Redis password
-	],
+    'Redis'     => [
+        'host'      => 'localhost',        // Redis server hostname
+        'port'      => 6379,                // Redis server port
+        'database'  => 0,            // Redis database number
+        'namespace' => 'resque',    // Redis keys namespace
+        'password'  => null            // Redis password
+    ],
 
-	'Worker'    => [
-		'queue'    => 'default',        // Name of the default queue
-		'interval' => 5,            // Number of second between each poll
-		'workers'  => 1,                // Number of workers to create
-		// 'user' => 'www-data' 	// User running the worker process
+    'Worker'    => [
+        'queue'    => 'default',        // Name of the default queue
+        'interval' => 5,            // Number of second between each poll
+        'workers'  => 1,                // Number of workers to create
+        // 'user' => 'www-data' 	// User running the worker process
 
-		// Path to the log file
-		// Can be an
-		// - absolute path,
-		// - an relative path, that will be relative to
-		// 	 app/tmp/logs folder
-		// - a simple filename, file will be created inside app/tmp/logs
-		'log'      => TMP . 'logs' . DS . 'resque-worker-error.log',
+        // Path to the log file
+        // Can be an
+        // - absolute path,
+        // - an relative path, that will be relative to
+        // 	 app/tmp/logs folder
+        // - a simple filename, file will be created inside app/tmp/logs
+        'log'      => TMP . 'logs' . DS . 'resque-worker-error.log',
 
-		// Log Verbose mode
-		// true to log more debugging informations
-		// Can also be enabled per worker, by starting with --verbose
-		'verbose'  => false,
-	],
-	'Job'       => [
-		// Whether to track job status
-		// Enabling this will allow you to track a job status by its ID
-		// Job status are purged after 24 hours
-		//
-		// You can also define per-job tracking by passing true/false when calling
-		// CakeResque::enqueue(), CakeResque::enqueueAt() or CakeResque::enqueueIn()
-		'track' => false,
-	],
-	/*
-	'Queues' => array(
-		array(
-			'queue' => 'default',	// Use default values from above for missing interval and count indexes
-			'user' => 'www-data'	// If PHP is running as a different user on you webserver
-		),
-		array(
-			'queue' => 'my-second-queue',
-			'interval' => 10
-		)
-	)
-	*/
-	'Resque'    => [
-		// Path to the directory containing the worker PID files
-		'tmpdir' => Plugin::path('CakeResque') . 'tmp' . DS,
-	],
+        // Log Verbose mode
+        // true to log more debugging informations
+        // Can also be enabled per worker, by starting with --verbose
+        'verbose'  => false,
+    ],
+    'Job'       => [
+        // Whether to track job status
+        // Enabling this will allow you to track a job status by its ID
+        // Job status are purged after 24 hours
+        //
+        // You can also define per-job tracking by passing true/false when calling
+        // CakeResque::enqueue(), CakeResque::enqueueAt() or CakeResque::enqueueIn()
+        'track' => false,
+    ],
+    /*
+    'Queues' => array(
+        array(
+            'queue' => 'default',	// Use default values from above for missing interval and count indexes
+            'user' => 'www-data'	// If PHP is running as a different user on you webserver
+        ),
+        array(
+            'queue' => 'my-second-queue',
+            'interval' => 10
+        )
+    )
+    */
+    'Resque'    => [
+        // Path to the directory containing the worker PID files
+        'tmpdir' => Plugin::path('CakeResque') . 'tmp' . DS,
+    ],
 
-	// Other usefull environment variable you wish to set
-	// Passing a key only will search for its value in the $_SERVER scope
-	// eg : array('SERVER_NAME'); => will search for the value in $_SERVER['SERVER_NAME']
-	// Passing a key and a value will set the env variable to this value
-	// eg : array('ARCH' => 'x64')
-	'Env'       => [],
+    // Other usefull environment variable you wish to set
+    // Passing a key only will search for its value in the $_SERVER scope
+    // eg : array('SERVER_NAME'); => will search for the value in $_SERVER['SERVER_NAME']
+    // Passing a key and a value will set the env variable to this value
+    // eg : array('ARCH' => 'x64')
+    'Env'       => [],
 
-	// Log Handler
-	// If saving the logs in a plain text file doesn't suit you
-	// you can send them to Mysql, or MongoDB, etc ...
-	// In that case, you'll need a handler to manage your logs
-	// All logs outputted by resque will go to the handler.
-	// The classic log file (above) will still be used, for logging
-	// stuff likes php error, or other STDOUT outputted by your job classses
-	//
-	// php-resque-ex uses Monolog to manage all the logging stuff
-	// If you uses the original php-resque library, these settings
-	// will be ignored
-	//
-	// handler
-	//		Name of the Handler (the handler classname, without the 'Handler' part)
-	// target
-	//		Arguments taken by the handler constructor. If the handler required
-	//		multiple arguments, separate them with a comma
-	//
-	// As of now, the following handler are supported:
-	//
-	// [HANDLER]		[TARGET]
-	// Cube 			Cube server address (e.g: udp://127.0.0.1:1180)
-	// RotatingFile 	Path to the log file (e.g: /path/to/resque.log)
-	// Syslog 			Facility name
-	// Socket 			Address (e.g: udp://127.0.0.1:23)
-	// MongoDB 			MongoDB server address  (e.g: mongodb://localhost:27017)
-	'Log'       => [
-		'handler' => 'RotatingFile',
-		'target'  => TMP . 'logs' . DS . 'resque.log',
-	],
+    // Log Handler
+    // If saving the logs in a plain text file doesn't suit you
+    // you can send them to Mysql, or MongoDB, etc ...
+    // In that case, you'll need a handler to manage your logs
+    // All logs outputted by resque will go to the handler.
+    // The classic log file (above) will still be used, for logging
+    // stuff likes php error, or other STDOUT outputted by your job classses
+    //
+    // php-resque-ex uses Monolog to manage all the logging stuff
+    // If you uses the original php-resque library, these settings
+    // will be ignored
+    //
+    // handler
+    //		Name of the Handler (the handler classname, without the 'Handler' part)
+    // target
+    //		Arguments taken by the handler constructor. If the handler required
+    //		multiple arguments, separate them with a comma
+    //
+    // As of now, the following handler are supported:
+    //
+    // [HANDLER]		[TARGET]
+    // Cube 			Cube server address (e.g: udp://127.0.0.1:1180)
+    // RotatingFile 	Path to the log file (e.g: /path/to/resque.log)
+    // Syslog 			Facility name
+    // Socket 			Address (e.g: udp://127.0.0.1:23)
+    // MongoDB 			MongoDB server address  (e.g: mongodb://localhost:27017)
+    'Log'       => [
+        'handler' => 'RotatingFile',
+        'target'  => TMP . 'logs' . DS . 'resque.log',
+    ],
 
-	// Scheduler Worker
-	// It's the worker handling all the scheduled jobs
-	// Only one scheduler worker is permitted to run at one time
-	// It can be paused, resumed and stopped like any other workers
-	// It can be started only with the `startscheduler` command,
-	// or with `load` if Scheduler Worker is enabled.
-	//
-	// Scheduled jobs requires the php-resque-ex-scheduler library,
-	// that should be installed with automatically via the
-	// `composer update` or `composer install` command
-	//
-	// The Scheduler Worker have its own default settings
-	//
-	// @since 2.3.0
-	//
-	'Scheduler' => [
-		// Enable or disable delayed job
-		'enabled' => false,
+    // Scheduler Worker
+    // It's the worker handling all the scheduled jobs
+    // Only one scheduler worker is permitted to run at one time
+    // It can be paused, resumed and stopped like any other workers
+    // It can be started only with the `startscheduler` command,
+    // or with `load` if Scheduler Worker is enabled.
+    //
+    // Scheduled jobs requires the php-resque-ex-scheduler library,
+    // that should be installed with automatically via the
+    // `composer update` or `composer install` command
+    //
+    // The Scheduler Worker have its own default settings
+    //
+    // @since 2.3.0
+    //
+    'Scheduler' => [
+        // Enable or disable delayed job
+        'enabled' => false,
 
-		// Path to the log file
-		'log'     => TMP . 'logs' . DS . 'resque-scheduler-error.log',
+        // Path to the log file
+        'log'     => TMP . 'logs' . DS . 'resque-scheduler-error.log',
 
-		// Optional
-		// Will not default to settings defined in the global scope above
-		'Env'     => [],
+        // Optional
+        // Will not default to settings defined in the global scope above
+        'Env'     => [],
 
-		// Optional
-		// Will default to settings defined in the global scope above
-		// Only available setting is `interval`
-		// The worker will always poll a fixed special queue, and only one worker can run at one time
-		'Worker'  => [
-			'interval' => 3,
-		],
+        // Optional
+        // Will default to settings defined in the global scope above
+        // Only available setting is `interval`
+        // The worker will always poll a fixed special queue, and only one worker can run at one time
+        'Worker'  => [
+            'interval' => 3,
+        ],
 
-		// Optional
-		// Will default to settings defined in the global scope above
-		'Log'     => [
-			'handler' => 'RotatingFile',
-			'target'  => TMP . 'logs' . DS . 'resque-scheduler.log',
-		],
-	],
+        // Optional
+        // Will default to settings defined in the global scope above
+        'Log'     => [
+            'handler' => 'RotatingFile',
+            'target'  => TMP . 'logs' . DS . 'resque-scheduler.log',
+        ],
+    ],
 ];
