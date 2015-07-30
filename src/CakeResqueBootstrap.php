98dfaa5086d81f1a0ad61a9c45fb5f76cd8b3e37
Marco Manieri <marco@italiatekno.it>
2015-07-30 17:17:55 +0200

    Final patches for cakePHP 3
    - Resque_Job_Creator.php is no longer needed, Resque_Job_Creator is defined in CakeResqueBootstrap.php
    - Resque_Job_Creator does not need to inspect anything since everything is nicely handled by the Cake\Console\ShellDispatcher->dispatch method
    - the args list must mock the 0th element from cli (file name: 'void')

--- src/CakeResqueBootstrap.php
+++ src/CakeResqueBootstrap.php
@@ -18,10 +18,30 @@
  * @since         0.5
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
-use Cake\Console\ShellDispatcher;
-use Cake\Console\Shell;
-REQUIRE 'Resque_Job_Creator.php';
-
 define('APP', getenv('APP'));
+include APP . '../config/bootstrap.php';
+use Cake\Console\ShellDispatcher;
 
-new ShellDispatcher($argv);
+class Resque_Job_Creator
+{
+
+    /**
+     * Create and return a job instance
+     *
+     * @param string $className className of the job to instanciate
+     * @param array $args Array of method name and arguments used to build the job
+     * @return object $args a job instance
+     * @throws Resque_Exception when the class is not found, or does not follow the job file convention
+     */
+    public static function createJob($className, $args)
+    {
+        if (!class_exists('Cake\Console\ShellDispatcher')) {
+            throw new Resque_Exception('Resque_Job_Creator could not find Cake\Console\ShellDispatcher.');
+        }
+
+        array_unshift($args, 'void', $className);
+
+        return [new Cake\Console\ShellDispatcher($args),'dispatch'];
+
+    }
+}
